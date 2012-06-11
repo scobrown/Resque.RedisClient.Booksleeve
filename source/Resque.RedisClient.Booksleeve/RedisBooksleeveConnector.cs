57def60217192f5de3e5a84a47692843bf1fbe3f
Scott Brown <scott.brown@secondsupper.com>
2012-06-11 09:28:12 -0400

    update to latest resque and add specs

--- source/Resque.RedisClient.Booksleeve/RedisBooksleeveConnector.cs
+++ source/Resque.RedisClient.Booksleeve/RedisBooksleeveConnector.cs
@@ -1,44 +1,78 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
 using BookSleeve;
 
 namespace Resque.RedisClient.Booksleeve
 {
     public class RedisBooksleeveConnector :IRedis
     {
+        public string RedisNamespace { get; set; }
         public RedisConnection Client { get; set; }
+        public int RedisDb { get; set; }
 
-        public RedisBooksleeveConnector(RedisConnection client)
+        public RedisBooksleeveConnector(RedisConnection client, int redisDb = 0, string redisNamespace = "resque")
         {
             Client = client;
+            RedisDb = redisDb;
+            RedisNamespace = redisNamespace;
+        }
+        public string KeyInNamespace(string key)
+        {
+            return string.Join(":", RedisNamespace, key);
+        }
+        public string[] KeyInNamespace(params string[] keys)
+        {
+            return keys.Select(x => string.Join(":", RedisNamespace, x)).ToArray();
         }
 
         public bool SAdd(string key, string redisId)
         {
-            return Client.Wait(Client.Sets.Add(0, key, redisId));
+            return Client.Wait(Client.Sets.Add(RedisDb, KeyInNamespace(key), redisId));
         }
 
         public string LPop(string key)
         {
-            return Client.Wait(Client.Lists.RemoveFirstString(0, key));
+            return Client.Wait(Client.Lists.RemoveFirstString(RedisDb, KeyInNamespace(key)));
+        }
+        public Tuple<string,string> BLPop(string[] keys, int timeoutSeconds = 0)
+        {
+            return Client.Wait(Client.Lists.BlockingRemoveFirstString(RedisDb, KeyInNamespace(keys), timeoutSeconds));
+        }
+
+        public IEnumerable<string> SMembers(string key)
+        {
+            return Client.Wait(Client.Sets.GetAllString(RedisDb, KeyInNamespace(key)));
+        }
+
+        public bool Exists(string key)
+        {
+            return Client.Wait(Client.Keys.Exists(RedisDb, KeyInNamespace(key)));
+        }
+
+        public string Get(string key)
+        {
+            return Client.Wait(Client.Strings.GetString(RedisDb, KeyInNamespace(key)));
         }
 
         public void Set(string key, string value)
         {
-            Client.Wait(Client.Strings.Set(0, key, value));
+            Client.Wait(Client.Strings.Set(RedisDb, KeyInNamespace(key), value));
         }
 
         public long RemoveKeys(params string[] keys)
         {
-            return Client.Wait(Client.Keys.Remove(0, keys));
+            return Client.Wait(Client.Keys.Remove(RedisDb, KeyInNamespace(keys)));
         }
 
         public long SRemove(string key, params string[] values)
         {
-            return Client.Wait(Client.Sets.Remove(0, key, values));
+            return Client.Wait(Client.Sets.Remove(RedisDb, KeyInNamespace(key), values));
         }
 
         public long RPush(string key, string value)
         {
-            return Client.Wait(Client.Lists.AddLast(0, key, value));
+            return Client.Wait(Client.Lists.AddLast(RedisDb, KeyInNamespace(key), value));
         }
     }
 }
---
+++ source/Specs/ResqueCanQueue.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using BookSleeve;
+using NUnit.Framework;
+using Resque;
+using Resque.FailureBackend;
+using Resque.RedisClient.Booksleeve;
+
+namespace Specs
+{
+    [TestFixture]
+    public class ResqueCanQueue
+    {
+        class TestPackage
+        {
+            public Resque.Resque UnderTest;
+            public RedisBooksleeveConnector Redis;
+
+            public TestPackage(RedisConnection conn)
+            {
+                Redis = new RedisBooksleeveConnector(conn);
+                var redisFS = new Resque.FailureService(new RedisBackendFactory(Redis));
+                UnderTest = new Resque.Resque(new JobCreator(), redisFS, Redis);
+
+
+                conn.Wait(conn.Keys.Remove(0, Redis.KeyInNamespace("queue:testQueue")));
+            }
+        }
+        [Test]
+        public void should_create_queue_if_it_does_not_exist()
+        {
+            var testQueue = "testQueue";
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                var package = new TestPackage(conn);
+
+                package.UnderTest.Push(testQueue, "Testing");
+
+                Assert.IsTrue(conn.Wait(conn.Keys.Exists(0, package.Redis.KeyInNamespace("queue:"+testQueue))));
+            }
+        }
+        [Test]
+        public void should_add_item_to_queue()
+        {
+            var testQueue = "testQueue";
+            var item = new QueuedItem()
+                               {
+                                   @class = "Testing"
+                               };
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                var package = new TestPackage(conn);
+                package.UnderTest.Push(testQueue, item.@class, item.args);
+
+                var test = conn.Wait(conn.Lists.RemoveLastString(0, package.Redis.KeyInNamespace("queue:" + testQueue)));
+
+                Assert.AreEqual(item.ToJson(), test);
+            }
+        }
+
+        [Test]
+        public void should_be_able_to_get_an_enqueued_item()
+        {
+            var testQueue = "testQueue";
+            var item = new QueuedItem()
+            {
+                @class = Guid.NewGuid().ToString()
+            };
+            using (var conn = new RedisConnection("localhost"))
+            {
+                conn.Open();
+                var package = new TestPackage(conn);
+                package.UnderTest.Push(testQueue, item.@class, item.args);
+            
+                var multi = new MultiQueue(package.Redis, new[] {testQueue});
+                var popped = multi.Pop();
+                Assert.AreEqual(item.ToJson(), popped.Item2.ToJson());
+            }
+        }
+    }
+
+    public class JobCreator
+        : IJobCreator
+    {
+        public IJob CreateJob(IFailureService failureService, Worker worker, QueuedItem deserializedObject, string queue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
